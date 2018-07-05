4481528f36a18b02124f859ff0bb0e0e4c5c2c12
Shane Whitehead <obishiro@gmail.com>
2018-07-05 16:12:30 +1000

    Scripts

--- makerelease.sh
+++ makerelease.sh
@@ -1,4 +1,4 @@
-sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer
+#sudo xcode-select -s /Applications/Xcode-beta.app/Contents/Developer
 xcodebuild -version
 rm *.zip
 rm -rf Carthage
@@ -8,5 +8,5 @@ else
 	time carthage build --no-skip-current
 fi
 source makearchive.sh
-sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
+#sudo xcode-select -s /Applications/Xcode.app/Contents/Developer
 xcodebuild -version
